8617e7d850fb5140f53030db1957eefb9ebacc25
Jessie Tarrosa <creators.nuke@gmail.com>
2021-04-05 18:23:59 +0800

    fixed: directory to author github

--- src/Components/Github/index.js
+++ src/Components/Github/index.js
@@ -4,7 +4,7 @@ export const Github = (props) => {
   return (
     <div>
       <div className='author'>
-        <a href='https://github.com/majessietic' 
+        <a href='https://github.com/oreoseenoevil' 
           target='_blank' rel='noopener noreferrer' 
           title="Link to author's Github Profile"
         >
