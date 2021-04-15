c1818ba9926c13132b896b48f1937cb3e36c06cc
oreoseenoevil <creators.nuke@gmail.com>
2021-04-15 22:55:26 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React, { StrictMode } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 import { App } from '@Ui/App'
 import '@Style/styles.scss'
 
 render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />,
   document.getElementById('app')
 )
