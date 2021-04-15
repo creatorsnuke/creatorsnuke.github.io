28af41c7b05f6fd947d4ca03a589218a9c0beea3
oreoseenoevil <creators.nuke@gmail.com>
2021-04-15 21:51:10 +0800

    fixed: grammar

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -10,7 +10,7 @@ export function Banner () {
           typewriter.typeString('<span class="line"><span class="dollar"></span> hi there, i&apos;m jessie</span><br />')
           typewriter.typeString('<span class="line"><span class="dollar"></span> a web developer</span><br />')
           typewriter.typeString('<span class="line"><span class="dollar"></span> based in cebu</span><br />')
-          typewriter.typeString('<span class="line"><span class="dollar"></span> i lift heavy weights</span>')
+          typewriter.typeString('<span class="line"><span class="dollar"></span> love lifting heavy weights and coding</span>')
             .start()
         }}
       />
