c8f00f4161bdcfcb6a5f8f5da3fd40a86257aeaf
oreoseenoevil <creators.nuke@gmail.com>
2021-04-24 21:21:59 +0800

    feat: changed delay type speed

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -10,6 +10,9 @@ export function Banner () {
       <div className="stars3"></div>
       <div className="banner-terminal">
         <Typewriter
+          options={{
+            delay: 60
+          }}
           onInit={typewriter => {
             typewriter.typeString('<span class="line"><span class="dollar"></span> hi there, i&apos;m jessie</span><br />')
             typewriter.typeString('<span class="line"><span class="dollar"></span> a web developer</span><br />')
